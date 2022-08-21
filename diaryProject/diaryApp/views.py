33498b878e7720b50d44248451247b0d8e6f3d4b
syrius527 <83915027+syrius527@users.noreply.github.com>
2022-08-21 15:52:08 +0900

    Fishing (#23)
    * save
    
    * db에 일기 저장 성공
    
    * make random rendering space background
    
    * update random rendering space background
    
    * js to mjs
    
    * save
    
    * background -> base
    
    * delete background legacy
    
    * save
    
    * index conflict
    
    * index static edit
    
    * login signup integrate
    
    * going on fishing event
    
    * fishing animation
    
    Co-authored-by: ssoy0514 <badr1108@naver.com>

--- diaryProject/diaryApp/views.py
+++ diaryProject/diaryApp/views.py
@@ -6,35 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 # Create your views here.
-def signup(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        found_user = User.objects.filter(username=username)
-        if len(found_user):
-            error = "이미 존재하는 아이디입니다."
-            return render(request, "registration/signup.html", {"error": error})
-        new_user = User.objects.create_user(username=username, password=password)
-        auth.login(request, new_user)
-        return redirect("index")
-        
-    return render(request, "registration/signup.html")
-
-
-def login(request):
-    if request.method == "POST":
-        username=request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect("index")
-        error = "아이디 혹은 비밀번호가 틀립니다."
-        return render(request, "registration/login.html", {"error": error})
-
-    return render(request, "registration/login.html")
-
-
 def logout(request):
     auth.logout(request)
 
